d6cf3bc0eb8db95a1cc504ef7908f4277c3ebd0c
timbze <tim.bze@gmail.com>
2021-03-08 15:04:40 -0600

    Improve suspend script

--- suspendInSixtySeconds.py
+++ suspendInSixtySeconds.py
@@ -1,23 +1,20 @@
 import os
 import threading
-
 import PySimpleGUI as sg
 
 sg.theme('DarkAmber')  # Add a touch of color
-window_initialized = False
 cancelled = False
 seconds = [60]
 
 
-def f(f_stops: threading.Event):
+def suspend_timer():
     if cancelled:
         return
     elif seconds[0] > 0:
         seconds[0] = (seconds[0]-1)
 
-        threading.Timer(1, f, [f_stops]).start()
-        if window_initialized:
-            window['suspendMessage'].update(get_suspend_message())
+        threading.Timer(1, suspend_timer).start()
+        window['suspendMessage'].update(get_suspend_message())
     else:
         suspend_now()
 
@@ -25,10 +22,6 @@ def f(f_stops: threading.Event):
 def suspend_now():
     os.system('systemctl suspend')
     window.write_event_value('Cancel', '')
-
-
-f_stop = threading.Event()
-f(f_stop)
 
 
 def get_suspend_message():
@@ -42,8 +35,8 @@ layout = [[sg.Text('')],
           [sg.Text('')],
           [sg.Button('Cancel')]]
 
-window = sg.Window('Suspend Timer', layout)
-window_initialized = True
+window = sg.Window('Suspend Timer', layout).finalize()
+suspend_timer()
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
